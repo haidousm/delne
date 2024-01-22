0a9a259050868d7b186746a433e74db74f4beac8
haidousm <haidous.m@gmail.com>
2024-01-21 19:47:09 -0800

    get templ to work for basic table

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -19,6 +20,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/proxies/register", app.registerProxy)
 
 	router.HandlerFunc(http.MethodGet, "/", app.proxyRequest)
+
+	component := ProxyTable(*app.proxy)
+	router.Handler(http.MethodGet, "/ui/proxies", templ.Handler(component))
 
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, app.secureHeaders)
 	return standardMiddleware.Then(router)
