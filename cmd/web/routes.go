a9db12e8ec4a736d413c6fdf2bc92f821db03287
haidousm <haidous.m@gmail.com>
2024-01-25 19:44:32 -0800

    refactor code slightly, add todos to readme

---
+++ cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handler(http.MethodGet, "/admin/static/*filepath", http.StripPrefix("/static", fileServer))
+
+	router.HandlerFunc(http.MethodGet, "/admin/api/healthcheck", app.health)
+
+	router.HandlerFunc(http.MethodGet, "/admin/api/proxies", app.listProxies)
+	router.HandlerFunc(http.MethodPut, "/admin/api/proxies/:host", app.editProxy)
+	router.HandlerFunc(http.MethodPost, "/admin/api/proxies", app.registerProxy)
+
+	component := ProxyPage(*app.proxy)
+	router.Handler(http.MethodGet, "/admin/proxies", templ.Handler(component))
+	router.HandlerFunc(http.MethodGet, "/admin/proxies/:host/edit", app.editProxyForm)
+	router.HandlerFunc(http.MethodGet, "/admin/proxy/new", app.createProxyForm)
+
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest) // app.secureHeaders
+	return standardMiddleware.Then(router)
+}
