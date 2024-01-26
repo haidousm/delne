c1e2f24add167fd29af188a233bc14dc45e8f84e
haidousm <haidous.m@gmail.com>
2024-01-25 21:03:44 -0800

    add container orchestration, additional tests

--- internal/docker/docker_test.go
+++ internal/docker/docker_test.go
@@ -1,6 +1,10 @@
 package docker
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/docker/docker/api/types/container"
+)
 
 func TestPullImage(t *testing.T) {
 	client, err := NewClient()
@@ -29,4 +33,99 @@ func TestPullImage(t *testing.T) {
 	}
 
 	t.Log("Image pulled successfully")
+}
+
+func TestCreateContainer(t *testing.T) {
+
+	client, err := NewClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	image := Image{
+		Repository: "_",
+		Name:       "alpine",
+		Tag:        "latest",
+	}
+
+	service := Service{
+		Name:    "test-alpine",
+		Image:   image,
+		Network: "test-network",
+	}
+
+	resp, err := client.CreateContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.ID == "" {
+		t.Fatal("Container ID not found")
+	}
+
+	t.Log("Container created successfully")
+	service.ContainerId = resp.ID
+	insp, err := client.inspectContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if insp.HostConfig.NetworkMode != container.NetworkMode(service.Network) {
+		t.Fatal("Container network not set")
+	}
+
+	t.Log("Container network set successfully")
+	err = client.RemoveContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestStartContainer(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	image := Image{
+		Repository: "_",
+		Name:       "alpine",
+		Tag:        "latest",
+	}
+
+	service := Service{
+		Name:    "test-alpine",
+		Image:   image,
+		Network: "test-network",
+	}
+
+	resp, err := client.CreateContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.ID == "" {
+		t.Fatal("Container ID not found")
+	}
+
+	t.Log("Container created successfully")
+	service.ContainerId = resp.ID
+	err = client.StartContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Container started successfully")
+	err = client.StopContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Container stopped successfully")
+	err = client.RemoveContainer(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Container removed successfully")
 }
